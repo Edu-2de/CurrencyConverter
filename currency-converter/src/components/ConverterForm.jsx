f2948a150d1ce8ea4d4d88e40abb9403da59d060
Eduardo Paim <zeedu223@gmail.com>
2025-04-16 11:39:56 -0300

    feat: initialize currency converter app with React and Vite
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application layout and components.
    - Developed App.jsx as the main component with basic state management and UI.
    - Added React logo SVG in the assets directory.
    - Implemented ConverterForm component for currency conversion functionality.
    - Styled the application with index.css for global styles.
    - Set up main.jsx for rendering the application.
    - Configured Vite with vite.config.js for building and serving the app.

---
+++ currency-converter/src/components/ConverterForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import '../styles/TaskForm.css';
+
+
+const ConvertForm = () => {
+  const [amount, setAmount] = useState('');
+  const [fromCurrency, setFromCurrency] = useState('');
+  const [toCurrency, setToCurrency] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleConvert = async (e) => {
+    e.preventDefault();
+    if (fromCurrency === toCurrency) return;
+
+    const response = await fetch(`https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}&amount=${amount}`);
+    const data = await response.json();
+    setResult(data.result);
+
+  };
+
+  return (
+    <form className="task-form" onSubmit={handleConvert}>
+      <input
+        type="number"
+        className="amount-input"
+        placeholder="Valor"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <select  
+       className="select-input"
+       placeholder="De"
+       value={fromCurrency}
+       onChange={(e) => setFromCurrency(e.target.value)}
+      >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+      </select>
+      <select  
+       className="select-input"
+       placeholder="Para"
+       value={toCurrency}
+       onChange={(e) => setToCurrency(e.target.value)}
+      >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+      </select>
+   
+      <input
+       type="number"
+       readOnly
+       className="result-input"
+       placeholder="Resultado"
+       value={result}
+      />
+      <button type="submit" className="add-button">Adicionar</button>
+    </form>
+  );
+};
+
+export default ConvertForm;
